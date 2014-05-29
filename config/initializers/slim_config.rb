c531142f2778e30ec1c3622bdee09aa4939aeac2
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-05-29 17:03:17 +0400

    roles_and_js_log

--- config/initializers/slim_config.rb
+++ config/initializers/slim_config.rb
@@ -1,1 +1,8 @@
-Slim::Engine.set_default_options pretty: true, sort_attrs: false
+Slim::Engine.set_default_options(pretty: true,
+  sort_attrs: false,
+  shortcut: {
+    '@' => { :attr => 'data-role' },
+    '#' => { :attr => 'id'    },
+    '.' => { :attr => 'class' }
+  }
+)
